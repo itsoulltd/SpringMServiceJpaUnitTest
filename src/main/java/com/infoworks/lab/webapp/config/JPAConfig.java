f0fd204fc43deab3e9d7667bf20cff5a4623b2a3
Towhidul Islam <towhidul.islam@nagad.com.bd>
2022-01-13 11:26:03 +0600

    refactoring

--- src/main/java/com/infoworks/lab/webapp/config/JPAConfig.java
+++ src/main/java/com/infoworks/lab/webapp/config/JPAConfig.java
@@ -1,12 +1,14 @@
 package com.infoworks.lab.webapp.config;
 
+import com.infoworks.lab.jsql.ExecutorType;
+import com.infoworks.lab.jsql.JsqlConfig;
+import com.it.soul.lab.sql.SQLExecutor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.*;
+import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
@@ -22,7 +24,13 @@ import javax.sql.DataSource;
         basePackages = {"com.infoworks.lab.domain.repositories"}
 )
 @PropertySource("classpath:mysql-db.properties")
-public class PrimaryJPAConfig {
+public class JPAConfig {
+
+    private Environment env;
+
+    public JPAConfig(Environment env) {
+        this.env = env;
+    }
 
     @Value("${spring.datasource.driver-class-name}")
     String driverClassName;
@@ -35,10 +43,20 @@ public class PrimaryJPAConfig {
     @Value("${app.db.name}")
     String persistenceUnitName;
 
-    @Primary
     @Bean
-    public DataSource dataSource(){
+    JsqlConfig getJsqlConfig(DataSource dataSource){
+        return new JsqlConfig(dataSource);
+    }
 
+    @Bean @Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE, proxyMode = ScopedProxyMode.TARGET_CLASS)
+    SQLExecutor executor(JsqlConfig config) throws Exception {
+        SQLExecutor exe = (SQLExecutor) config.create(ExecutorType.SQL, env.getProperty("app.db.name"));
+        System.out.println("Created DB Connections.");
+        return exe;
+    }
+
+    @Primary @Bean
+    public DataSource dataSource(){
         DataSource dataSource = DataSourceBuilder
                 .create()
                 .username(username)
@@ -49,12 +67,10 @@ public class PrimaryJPAConfig {
         return dataSource;
     }
 
-    @Primary
-    @Bean
+    @Primary @Bean
     public LocalContainerEntityManagerFactoryBean entityManagerFactory(
             EntityManagerFactoryBuilder builder
             , DataSource dataSource){
-
         return builder
                 .dataSource(dataSource)
                 .packages("com.infoworks.lab.domain.entities")
@@ -62,11 +78,9 @@ public class PrimaryJPAConfig {
                 .build();
     }
 
-    @Primary
-    @Bean
+    @Primary @Bean
     public PlatformTransactionManager transactionManager(
             EntityManagerFactory entityManagerFactory){
-
         return new JpaTransactionManager(entityManagerFactory);
     }
 
