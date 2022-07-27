8bd5618afd202ade7af6096707a2e4b5da80e6c6
Towhidul Islam <m.towhid.islam@gmail.com>
2022-07-27 18:13:51 +0600

    adding example of context refreshed and closed

---
+++ src/main/java/com/infoworks/lab/webapp/config/StartupConfig.java
@@ -0,0 +1,26 @@
+package com.infoworks.lab.webapp.config;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.event.ContextClosedEvent;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class StartupConfig implements CommandLineRunner {
+
+    @EventListener
+    public void handleContextStartedListener(ContextRefreshedEvent event){
+        System.out.println("ContextStarted....");
+    }
+
+    @EventListener
+    public void handleContextStoppedListener(ContextClosedEvent event){
+        System.out.println("ContextStopped....");
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Startup Done...");
+    }
+}
