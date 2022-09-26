addc27384c3d37ad39d3e0c63ff91fa2ab09efdd
Towhid Islam <m.towhid.islam@gmail.com>
2022-09-27 00:34:14 +0600

    controller-test: issue org.springframework.web.HttpMediaTypeNotSupportedException need to fix

--- src/test/java/com/infoworks/lab/controllers/rest/PassengerControllerUnitTest.java
+++ src/test/java/com/infoworks/lab/controllers/rest/PassengerControllerUnitTest.java
@@ -1,8 +1,8 @@
 package com.infoworks.lab.controllers.rest;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.infoworks.lab.domain.models.Gender;
 import com.infoworks.lab.domain.entities.Passenger;
+import com.infoworks.lab.domain.models.Gender;
 import com.infoworks.lab.services.iServices.PassengerService;
 import com.infoworks.lab.webapp.WebApplicationTest;
 import com.infoworks.lab.webapp.config.BeanConfig;
@@ -10,7 +10,6 @@ import com.infoworks.lab.webapp.config.TestJPAH2Config;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -26,12 +25,13 @@ import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK,
         classes = {WebApplicationTest.class, TestJPAH2Config.class, BeanConfig.class})
-@AutoConfigureMockMvc
+@AutoConfigureMockMvc(secure = false)
 @TestPropertySource(locations = "classpath:h2-db.properties")
 public class PassengerControllerUnitTest {
 
@@ -49,39 +49,19 @@ public class PassengerControllerUnitTest {
     @MockBean
     PassengerService service;
 
-    @InjectMocks
-    PassengerController controller;
+    /*@InjectMocks
+    PassengerController controller;*/
 
     @Test
     public void helloGetTest() throws Exception {
         //Call controller to make the save:
         MvcResult result = mockMvc.perform(get("/passenger/hello"))
-                //.andExpect(status().isOk())
+                //.andDo(print())
+                .andExpect(status().isOk())
                 .andReturn();
-        String str = "Status: " + result.getResponse().getStatus();
-        System.out.println(str);
+        //
         String responseBody = result.getResponse().getContentAsString();
         System.out.println(responseBody);
-        //
-    }
-
-    @Test
-    public void happyPathTest() throws Exception {
-        //Defining Mock Object:
-        Passenger aPassenger = new Passenger("Towhid", Gender.MALE, 36);
-
-        when(service.add(any(Passenger.class))).thenReturn(aPassenger);
-
-        //Call controller to make the save:
-        MvcResult result = mockMvc.perform(post("/passenger")
-                .accept(MediaType.APPLICATION_JSON)
-                .content(mapper.writeValueAsBytes(aPassenger))
-                .contentType(MediaType.APPLICATION_JSON_UTF8))
-                //.andExpect(status().isOk())
-                .andReturn();
-        String str = "Status: " + result.getResponse().getStatus();
-        System.out.println(str);
-        //
     }
 
     @Test
@@ -89,13 +69,34 @@ public class PassengerControllerUnitTest {
         //Call controller to make the save:
         MvcResult result = mockMvc.perform(get("/passenger/rowCount")
                 .accept(MediaType.APPLICATION_JSON)
-                .contentType(MediaType.APPLICATION_JSON_UTF8))
-                //.andDo(print())
+                .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andReturn();
+        //
+        String responseBody = result.getResponse().getContentAsString();
+        System.out.println(responseBody);
+    }
+
+    @Test
+    public void insertPostTest() throws Exception {
+        //Defining Mock Object:
+        Passenger aPassenger = new Passenger("Towhid", Gender.MALE, 36);
+        //Mock-Stabbing:
+        when(service.add(any(Passenger.class))).thenReturn(aPassenger);
+        //
+        String requestJson = mapper.writeValueAsString(aPassenger);
+        //Call controller to make the save:
+        MvcResult result = mockMvc.perform(post("/passenger")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(requestJson)
+                .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
                 //.andExpect(status().isOk())
                 .andReturn();
-        String str = "Status: " + result.getResponse().getStatus();
-        System.out.println(str);
         //
+        String responseBody = result.getResponse().getContentAsString();
+        System.out.println(responseBody);
     }
 
 }
