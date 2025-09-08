851382ab4a630bdb5ff558eead68db38e5fbee98
DEGRUYTER\towhid.islam <towhid.islam@degruyter.com>
2025-09-08 12:33:14 +0200

    test: fix and updated

--- src/test/java/com/infoworks/lab/domain/repositories/TemplateRepositoryUnitTest.java
+++ src/test/java/com/infoworks/lab/domain/repositories/TemplateRepositoryUnitTest.java
@@ -1,35 +1,47 @@
 package com.infoworks.lab.domain.repositories;
 
+import com.infoworks.lab.webapp.WebApplicationTest;
+import com.infoworks.lab.webapp.config.BeanConfig;
 import com.infoworks.lab.webapp.config.TestJPAH2Config;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = {TestJPAH2Config.class})
+@SpringBootTest(classes = {WebApplicationTest.class, BeanConfig.class, TestJPAH2Config.class})
 @Transactional
 @TestPropertySource(locations = {"classpath:application-h2db.properties"})
 public class TemplateRepositoryUnitTest {
 
-    /*@Autowired
-    TemplateRepository repository;*/
+    @Autowired /*@Qualifier("<bean-name>")*/ /*Or replace JpaRepository with actual @Repository declaration.*/
+    JpaRepository repository;
 
     @Test
-    public void insert(){}
+    public void initTest() {
+        Assert.assertNotNull(repository);
+        System.out.println("JpaRepository loaded: " + repository);
+    }
 
     @Test
-    public void update(){}
+    public void insert() {}
 
     @Test
-    public void delete(){}
+    public void update() {}
 
     @Test
-    public void count(){}
+    public void delete() {}
 
     @Test
-    public void fetch(){}
+    public void count() {}
+
+    @Test
+    public void fetch() {}
 
 }
