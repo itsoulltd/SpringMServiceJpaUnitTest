3c8b964bf545ec7a1fea453c5e7346c4250e4924
Towhid Islam <m.towhid.islam@gmail.com>
2026-07-31 23:08:44 +0200

    updated

--- src/main/java/com/infoworks/lab/domain/entities/Persistable.java
+++ src/main/java/com/infoworks/lab/domain/entities/Persistable.java
@@ -8,7 +8,7 @@ import javax.persistence.*;
 public class Persistable<ID, VERSION> extends Entity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private ID id;
 
     @Version @JsonIgnore
